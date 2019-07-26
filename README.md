0e901f7854612ebad2c5fa124976cc68f637c179
lihan <tclh123@gmail.com>
2019-07-26 14:42:13 +0800

    publish to pypi as pydnspod2

--- README.md
+++ README.md
@@ -1,31 +1,39 @@
 # pydnspod
 
+![](https://img.shields.io/pypi/pyversions/sa-tools-core)
+
+pydnspod 是一个 DNSPod 国内版 API 的 Python SDK.
+
 [DNSPod API Docs](https://github.com/DNSPod/dnspod-api-doc/)
 
-#关于
+# Installation
 
-pydnspod是一个DNSPod国内版API的Python SDK.
+```
+pip install pydnspod2
+```
 
-#特性
+# 特性
 
 - 支持自定义User-Agent
 - 支持D令牌
 - 发送请求前检查是否遗漏参数，避免无效请求
 - 使用命名参数，可以单独指定特定api的返回结果的format、lang
 
-#使用方法
+# 使用方法
 
-    from pydnspod import pydnspod
+```python
+from pydnspod import pydnspod
 
-    if __name__ == "__main__":
-        login_email = ''
-        login_password = ''
-        api = pydnspod.Api(login_email, login_password)
-        print api.version()
+if __name__ == "__main__":
+    login_email = ''
+    login_password = ''
+    api = pydnspod.Api(login_email, login_password)
+    print api.version()
 
-        user_manager=pydnspod.User(login_email, login_password)
-        print user_manager.detail()
+    user_manager=pydnspod.User(login_email, login_password)
+    print user_manager.detail()
 
-        domain_manager = pydnspod.Domain(login_email, login_password)
-        print domain_manager.create(domain="domain.name")
-        print domain_manager.info(domain="domain.name")
+    domain_manager = pydnspod.Domain(login_email, login_password)
+    print domain_manager.create(domain="domain.name")
+    print domain_manager.info(domain="domain.name")
+```
--- setup.py
+++ setup.py
@@ -1,20 +1,42 @@
 #!/usr/bin/python
 # coding=utf-8
 
-from distutils.core import setup
+from io import open
+from setuptools import setup, find_packages
 from pydnspod import __version__
 
 
 setup(
-    name='pydnspod',
+    name='pydnspod2',
     version=__version__,
     description='pydnspod - a dnspod api sdk',
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
     author='solos',
     author_email='lxl1217@gmail.com',
-    packages=["pydnspod"],
-    license="MIT",
+    maintainer='Harry Lee',
+    maintainer_email='tclh123@gmail.com',
+    url='https://github.com/tclh123/pydnspod',
+    keywords=['DNSPod', 'DNS', 'SDK'],
+    license="MIT License",
     platforms=["any"],
+    packages=find_packages(exclude=['examples*', 'tests*']),
+    include_package_data=True,
+    zip_safe=False,
     install_requires=['six'],
-    url='https://github.com/solos/pydnspod'
 )
